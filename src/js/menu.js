81e92472537c02bf68610aa5dec11d0ab45b4eb7
Raúl Marty <santor3medio@gmail.com>
2018-02-09 21:01:16 -0600

    #1. Add the actions of the navbar menu in menu.js

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,3 +1,53 @@
+var last_known_scroll_position = 0;
+var ticking = false;
+
+var siteNavbar = document.querySelector('.site-navbar');
+var phoneNavbar = document.querySelector('.navbar-phone-container');
+var decoratorLink = document.querySelector('.link-decorator');
+var textLink = document.querySelector('.navbar-option-link');
+
+//navbar-fixed-top
+function doSomething(scroll_pos) {
+  if (window.location.pathname === '/index.html' || window.location.pathname === '/' || window.location.pathname === '/project-wicore/index.html' || window.location.pathname === '/project-wicore/') {
+    if (scroll_pos > 30) {
+      siteNavbar.classList.add('nav-strongblue');
+      siteNavbar.classList.remove('nav-transparent');
+      phoneNavbar.classList.add('phone-no-shadow');
+      phoneNavbar.classList.remove('phone-shadow');
+      decoratorLink.classList.remove('link-decorator');
+      $('.link-decorator').addClass('remove-decorator');
+      //textLink.classList.add('active-link-scroll');
+
+    } else {
+      siteNavbar.classList.add('nav-transparent');
+      siteNavbar.classList.remove('nav-strongblue');
+      phoneNavbar.classList.add('phone-shadow');
+      phoneNavbar.classList.remove('phone-no-shadow');
+      decoratorLink.classList.add('link-decorator');
+      $('.link-decorator').removeClass('remove-decorator');
+      $('.navbar-option-link').removeClass('active-link-scroll');
+    }
+  }
+}
+
+doSomething();
+
+window.addEventListener('scroll', function(e) {
+  if (e.view) {
+    last_known_scroll_position = e.view.pageYOffset;
+  }
+  else {
+    last_known_scroll_position = window.scrollY;
+  }
+  if (!ticking) {
+    window.requestAnimationFrame(function() {
+      doSomething(last_known_scroll_position);
+      ticking = false
+    });
+  }
+  ticking = true;
+});
+
 function showMenu() {
   document.querySelector('#menuContainer').style.width = '85%';
   document.querySelector('#hamburgerMenu').style.display = 'none';
