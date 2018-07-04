78a671ad5d1ed0ccca8132162877d6b424cd6229
Raúl Marty <santor3medio@gmail.com>
2018-07-04 09:49:14 -0500

    #18. Duplicate the integration form to the contact view to the mobile version in the script file contact.js

--- src/js/contact.js
+++ src/js/contact.js
@@ -26,3 +26,32 @@ $('form[name="wicore-form"]').validate({
    alertify.error("Verifica tu información tienes " + errors + " errores.");
   }
 });
+
+$('form[name="wicore-form-mobile"]').validate({
+  name: 'required',
+  email: 'required',
+  messages: {
+   name: "Por favor, introduce tu Nombre.",
+   email: "Por favor, introduce un Correo Electrónico.",
+  },
+  submitHandler: function(form) {
+   var data = $('form[name="wicore-form-mobile"]').serialize();
+   console.log(data);
+   $.ajax({
+     url: 'http://integrations.blick.mx/wicore/contact/',
+     method: 'POST',
+     data: data
+   }).done(function(data) {
+     if (parseInt(data) === 1) {
+       alertify.logPosition("bottom right");
+       alertify.success("Correo enviado, gracias por contactarte con nosotros.");
+       $('form[name="wicore-form-mobile"]')[0].reset();
+     }
+   });
+  },
+  invalidHandler: function(event, validator) {
+   var errors = validator.numberOfInvalids();
+   alertify.logPosition("bottom right");
+   alertify.error("Verifica tu información tienes " + errors + " errores.");
+  }
+});
